aa49a2bb6512f5835c29c2c54774d6c5d11432a8
Galuh Idam Danutirto <galuh.danutirto@ahp.id>
2024-08-22 10:13:29 +0700

    bugfix: fix some issues

--- src/app/shared/filter-bar/dto/filter-bar.dto.ts
+++ src/app/shared/filter-bar/dto/filter-bar.dto.ts
@@ -2,11 +2,52 @@ import { FormControl } from '@angular/forms';
 import { SelectableDTO } from '@quantum/fui';
 
 export interface FilterBarDTO {
-  label: string;
-  status: boolean;
-  formControl: FormControl;
-  placeholder?: string;
-  options: SelectableDTO[];
-  selected: SelectableDTO[];
-  type: 'combo-box' | 'text' | 'dateRange' | 'datePicker' | 'time-selection';
+  formControlWhere: FormControl;
+  optionsWhere: {
+    name: string;
+    value: any;
+  }[];
+  selectedWhere: {
+    name: string;
+    value: any;
+  }[];
+
+  formControlColumn: FormControl;
+  optionsColumn: {
+    name: string;
+    value: any;
+  }[];
+  selectedColumn: {
+    name: string;
+    value: any;
+  }[];
+
+  formControlCondition: FormControl;
+  optionsCondition: {
+    name: string;
+    value: any;
+  }[];
+  selectedCondition: {
+    name: string;
+    value: any;
+  }[];
+
+  formControlValue: FormControl;
+  optionsValue: {
+    name: string;
+    value: any;
+  }[];
+  selectedValue: {
+    name: string;
+    value: any;
+  }[];
+
+  typeForm: 'combo-box' | 'text' | 'datePicker' | 'time-selection';
+}
+
+export interface OutputFilterBar {
+  where: string;
+  column: string;
+  condition: string;
+  value: string;
 }
